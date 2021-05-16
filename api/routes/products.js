4d90beec79ba36d4037982f71846732301d97632
Bardiesel <bardiamarandi@hotmail.com>
2021-05-17 01:40:04 +0430

    add delete router

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,30 +1,37 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        msg: 'Get from Products'
-    });
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    msg: "Get from Products",
+  });
 });
 
-router.post('/', (req, res, next) => {
-    res.status(200).json({
-        msg: 'Post from Products'
-    });
+router.post("/", (req, res, next) => {
+  res.status(200).json({
+    msg: "Post from Products",
+  });
 });
 
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId
-    if (id === 'book') {
-        res.status(200).json({
-            msg: 'Book'
-        })
-    } else {
-        res.status(200).json({
-            msg: 'Product not Found!'
-        })
-    }
-})
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  if (id === "book") {
+    res.status(200).json({
+      msg: "Book",
+    });
+  } else {
+    res.status(200).json({
+      msg: "Product not Found!",
+    });
+  }
+});
 
-module.exports = router
+router.delete("/:productId", (req, res, next) => {
+  res.status(200).json({
+    msg: "Delete Product",
+    productId: req.params.productId,
+  });
+});
+
+module.exports = router;
