6d640a1642f1c41bebe7c4a236b562cd1ff57c47
Bardiesel <bardiamarandi@hotmail.com>
2021-05-18 17:35:19 +0430

    add cors set headers

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const orderRoutes = require("./api/routes/orders");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "*");
+    return res.status(200).json();
+  }
+  next();
+});
+
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
