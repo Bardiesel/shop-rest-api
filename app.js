9e955b16cc48ad537d11569b33de72feae3b4484
Bardiesel <bardiamarandi@hotmail.com>
2021-05-19 23:04:36 +0430

    add userRoutes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+const userRoutes = require("./api/routes/user");
 
 mongoose.connect("mongodb://localhost:27017/rest-api", {
   useNewUrlParser: true,
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
 
 app.use((req, res, next) => {
   res.status(404).json({
